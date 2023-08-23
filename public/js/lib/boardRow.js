0296170fb483dcae4c223ee600afab20a10528c0
sangh <sang969@naver.com>
2023-08-23 16:21:29 +0900

    fix: list.js, list.html 코드추가

--- public/js/lib/boardRow.js
+++ public/js/lib/boardRow.js
@@ -1,19 +1,21 @@
 class BoardRow {
-    id;
-    title;
-    content;
-    writer;
-    created_at;
-    hit;
-  
-    constructor(row) {
-      this.id = row.id;
-      this.title = row.title;
-      this.content = row.content;
-      this.writer = row.writer;
-      this.created_at = "2023-08-09";
-      this.hit = 0;
-    }
+  id;
+  title;
+  content;
+  writer;
+  created_at;
+  hit;
+
+  constructor(row) {
+    this.id = row.id;
+    this.title = row.title;
+    this.content = row.content;
+    this.writer = row.writer;
+    this.created_at = "2023-08-09";
+    this.hit = 0;
   }
-  
-  export default BoardRow;
+}
+
+const clock = document.getElementById("clock");
+
+export default BoardRow;
