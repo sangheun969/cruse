4d2ce3c1522c2ad9eef272fdc763472410f023dd
chamdom-blockchain <wjdtmdry88@gmail.com>
2023-08-24 10:56:06 +0900

    fix : 수정

--- public/js/lib/boardRow.js
+++ public/js/lib/boardRow.js
@@ -1,19 +1,20 @@
 class BoardRow {
-  id;
-  title;
-  content;
-  writer;
-  created_at;
-  hit;
-
-  constructor(row) {
-    this.id = row.id;
-    this.title = row.title;
-    this.content = row.content;
-    this.writer = row.writer;
-    this.created_at = row.createt_at;
-    this.hit = 0;
+    id;
+    title;
+    content;
+    writer;
+    created_at;
+    hit;
+  
+    constructor(row) {
+      this.id = row.id;
+      this.title = row.title;
+      this.content = row.content;
+      this.writer = row.writer;
+      this.created_at = new Date(Date.now()).toLocaleDateString()
+      this.hit = 0;
+    }
   }
-}
+
 
 export default BoardRow;
